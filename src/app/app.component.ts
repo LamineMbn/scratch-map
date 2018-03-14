fac7d292e46b6d8c6d79d0e8ae5eeead2b6ca575
LamineMbn <octave.wolfy@gmail.com>
2018-03-14 21:10:49 +0100

    Add boilerplate for map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AmChartsService } from '@amcharts/amcharts3-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+
+  constructor(readonly _amCharts: AmChartsService) {
+  }
+
+
+  ngOnInit(): void {
+    this._amCharts.makeChart('mapdiv', {
+      /**
+       * this tells amCharts it's a map
+       */
+      type: 'map',
+
+      /**
+       * create data provider object
+       * map property is usually the same as the name of the map file.
+       * getAreasFromMap indicates that amMap should read all the areas available
+       * in the map data and treat them as they are included in your data provider.
+       * in case you don't set it to true, all the areas except listed in data
+       * provider will be treated as unlisted.
+       */
+      dataProvider: {
+        map: 'worldHigh',
+        getAreasFromMap: true
+      },
+
+      /**
+       * create areas settings
+       * autoZoom set to true means that the map will zoom-in when clicked on the area
+       * selectedColor indicates color of the clicked area.
+       */
+      areasSettings: {
+        autoZoom: true,
+        selectedColor: '#CC0000'
+      },
+
+      /**
+       * let's say we want a small map to be displayed, so let's create it
+       */
+      smallMap: {}
+    });
+  }
 }
